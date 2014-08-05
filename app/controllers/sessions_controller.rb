2f321cbcdbe91b77dd58512a66791198fda6cc39
tjku <tomaszjankuc@gmail.com>
2014-08-05 20:06:38 +0200

    #6 Refactor session form for sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(email: params[:email].downcase)
 
-    if user and user.authenticate(params[:session][:password])
+    if user and user.authenticate(params[:password])
       sign_in user
       redirect_to user
     else
